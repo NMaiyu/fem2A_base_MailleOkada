07047f16aa90d2f4b3218ce8001c61d090993287
NMaiyu <naossa.nmo@gmail.com>
2024-04-17 09:58:45 +0200

    simulations : pure dirichlet and dirichlet with source

--- src/simu.h
+++ src/simu.h
@@ -59,7 +59,7 @@ namespace FEM2A {
                 local_to_global_matrix(M, t, Ke, K);
             }
             
-            std::vector< double > F(M.nb_vertices(), 1);
+            std::vector< double > F(M.nb_vertices(), 0);
             
             std::vector< bool > attribute_bool(1, true);
             std::vector< double > values(M.nb_vertices());
@@ -79,6 +79,68 @@ namespace FEM2A {
                 std::cout << i << ' ';
             }
             std::cout <<'\n';
+            
+            save_solution(x, "data/square.bb");
+            
+        }
+        
+        
+        
+        void source_dirichlet_pb( const std::string& mesh_filename, bool verbose )
+        {
+            std::cout << "Solving a Dirichlet problem with source" << std::endl;
+            if ( verbose ) {
+                std::cout << " with lots of printed details..." << std::endl;
+            }
+            
+            
+            // LOAD MESH AND SET ATTRIBUTES
+            Mesh M;
+            M.load(mesh_filename);
+            M.set_attribute( unit_fct, 0, true );
+            
+            // CHOOSE QUADRATURE AND SHAPE FUNCTIONS
+            ShapeFunctions fonctions(2,1);
+            Quadrature quadrat = Quadrature::get_quadrature(0,false);
+            
+            // CREATE EMPTY K, F
+            int t_max;
+            t_max = M.nb_vertices() * quadrat.nb_points();
+            DenseMatrix Ke ;
+            SparseMatrix K(t_max);
+            std::vector< double > Fe;
+            std::vector< double > F(t_max,0);
+            
+            
+            // CREATE MATRIX K AND VECTOR F
+            for (int t=0 ; t<M.nb_triangles(); ++t)
+            {
+                ElementMapping element(M, false, t);
+                // K
+                assemble_elementary_matrix(element, fonctions, quadrat, unit_fct, Ke);
+                local_to_global_matrix(M, t, Ke, K);
+                
+                // F
+                assemble_elementary_vector(element, fonctions, quadrat, unit_fct, Fe);
+                local_to_global_vector(M, false,t, Fe, F);
+            }            
+            
+            // CHOOSE AND APPLY DIRICHLET CONDITIONS
+            std::vector< bool > attribute_bool(1, true);
+            std::vector< double > values(M.nb_vertices(),0);
+            apply_dirichlet_boundary_conditions(M, attribute_bool, values, K, F);
+            
+            // SOLVE
+            std::vector< double > x(M.nb_vertices(), 0);
+            solve(K,F, x);
+            
+            for (double i :x)
+            {
+                std::cout << i << ' ';
+            }
+            std::cout <<'\n';
+            
+            save_solution(x, "data/square.bb");
             
         }
 
